04b6a434f84a4139cf437d3e8b2620b4dfdf0ee5
Ayush Verma <vermajiii8116@gmail.com>
2026-06-04 15:14:18 +0530

    latest

--- ollama/src/main/java/com/eazyBytes/openai/controller/ChatController.java
+++ ollama/src/main/java/com/eazyBytes/openai/controller/ChatController.java
@@ -4,19 +4,32 @@ import com.eazyBytes.openai.view.ChatRequestView;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.LinkedList;
+
 @RestController
 @RequestMapping("/api")
 public class ChatController {
 
     private final ChatClient chatClient;
+    private final LinkedList<String> messages;
 
     public ChatController(ChatClient.Builder chatClientBuilder) {
         this.chatClient = chatClientBuilder.build();
+        this.messages = new LinkedList<>();
     }
 
     @PostMapping("/chat")
     public String chat(@RequestBody ChatRequestView chatRequestView) {
-        String content = chatClient.prompt(chatRequestView.message()).call().content();
+
+        String content = chatClient
+                .prompt()
+                .system("""
+                        you have to act like a hotel manager and ask politely to buy lend the room of the hotel.
+                        """)
+                .user(chatRequestView.message())
+                .call()
+                .content();
+
         return content;
     }
 }
